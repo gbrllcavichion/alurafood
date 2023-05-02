c2a2ee17a00ee34e5a3cdf1ac5f4076b09fb4130
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-02 17:42:48 -0300

    configurando o projeto

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -0,0 +1,30 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import com.rabbitmq.client.*;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.*;
+import org.springframework.boot.context.event.*;
+import org.springframework.context.*;
+import org.springframework.context.annotation.*;
+
+
+@Configuration
+public class PagamentoAMQPConfiguration {
+
+    @Bean
+    public Queue criaFila(){
+        // return QueueBuilder.nonDurable("pagamento.concluido").build();
+        return new Queue("pagamento.concluido", false);
+    }
+
+    @Bean
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+}
